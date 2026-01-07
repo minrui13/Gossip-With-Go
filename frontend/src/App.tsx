da087f97348be9edc66e9ff3279e543baed0255c
min rui <gohminrui@gmail.com>
2026-01-07 09:31:32 +0800

    App.tsx - Add StrictMode, handle unauthorised page and page not found  #9

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./auth/Auth";
 import { RequireAuth } from "./auth/RequireAuth";
 import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
 import "./css/base.css";
+import "./css/index.css";
 import Profile from "./pages/Profile";
 import PageLayout from "./components/PageLayout";
 import { ToastContainer } from "react-toastify";
+import SignUp from "./pages/SignUp";
+import { StrictMode } from "react";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
-    <>
+    <StrictMode>
       <ToastContainer
         autoClose={5000}
         limit={5}
@@ -21,16 +25,16 @@ export default function App() {
         <AuthProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route element={<PageLayout />}>
               <Route path="/" element={<MainPage />} />
-              <Route
-                path="/profile"
-                element={<RequireAuth component={<Profile />} />}
-              />
             </Route>
+            <Route path="/404" element={<Error errorCode="404" />} />
+            <Route path="/401" element={<Error errorCode="401" />} />
+            <Route path="*" element={<Navigate to="/404" />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
-    </>
+    </StrictMode>
   );
 }
