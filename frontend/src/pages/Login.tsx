7ce0c127a37b19816be68c02860f78b72d3d1e3f
min rui <gohminrui@gmail.com>
2026-01-13 10:17:15 +0800

    Login - add passsword to login frontend  #3

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,13 @@
-import { TextField } from "@mui/material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
 import "../css/login.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BuzzBeeLogo from "../images/BuzzBee_Logo.PNG";
 import { useAuth } from "../auth/Auth";
 import axios from "axios";
 import Loading from "../components/Loading";
 import { Spinner } from "react-bootstrap";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { replace, useNavigate } from "react-router-dom";
 import Error from "../utils/Error";
 import handleError from "../utils/Error";
 
@@ -15,25 +15,37 @@ export default function Login() {
   //sign in method in useAuth
   const { signIn } = useAuth();
   //login input
-  const [loginInput, setLoginInput] = useState("");
+  const [loginInput, setLoginInput] = useState({
+    username: "",
+    password: "",
+  });
   //error text for username
   const [errorText, setErrorText] = useState("");
   //manage loading
   const [isLoading, setIsLoading] = useState(false);
+  //toggle password visibilty
+  const [showPasswordVisibility, setShowPasswordVisibility] = useState(false);
   //navigate user to another page
   const navigate = useNavigate();
 
   async function submitLogin() {
     setIsLoading(true);
     //Check if user input anythinf
-    if (loginInput.trim().length == 0) {
+    if (
+      loginInput.username.trim().length == 0 &&
+      loginInput.password.trim().length == 0
+    ) {
+      changeErrorText("Please input a username and a password");
+      return;
+    } else if (loginInput.username.trim().length == 0) {
       changeErrorText("Please input a username");
+      return;
+    } else if (loginInput.password.trim().length == 0) {
+      changeErrorText("Please input a password");
       return;
     }
     try {
       await signIn(loginInput);
-      //stop loading
-      setIsLoading(false);
       //use toast to notify users
       toast("Login successfully!", {
         autoClose: 4000,
@@ -60,6 +72,9 @@ export default function Login() {
           changeErrorText(err);
         },
       });
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
     }
   }
 
@@ -69,17 +84,11 @@ export default function Login() {
       setIsLoading(false);
       //display error text
       setErrorText(text);
-      //notify users
-      toast(text, {
-        autoClose: 3000,
-        type: "error",
-      });
     }, 800);
   }
 
   return (
-   
-    <div style={{ backgroundColor: "var(--milk-white)" }}> 
+    <div style={{ backgroundColor: "var(--milk-white)" }}>
       {<Loading isLoading={isLoading} />}
       <div
         className="d-flex align-items-center justify-content-center 
@@ -90,48 +99,140 @@ export default function Login() {
             Login <img src={BuzzBeeLogo} className="signup-login-logo-img" />
           </h1>
 
-          <div>
-            <p id="login-username-label" className="login-signup-label">
-              Username:{" "}
-            </p>
-            <TextField
-              error={errorText.trim().length > 0}
-              id="login_username_input"
-              size="small"
-              value={loginInput}
-              onChange={(e) => {
-                setLoginInput(e.target.value);
-              }}
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  fontFamily: "Segoe UI",
-                  "& .MuiOutlinedInput-notchedOutline": {
-                    borderColor:
-                      errorText.trim().length > 0
+          <div className="d-flex flex-column" style={{ gap: 5 }}>
+            {/*Username*/}
+            <div>
+              <p id="login-username-label" className="login-signup-label">
+                Username:{" "}
+              </p>
+              <TextField
+                disabled={isLoading}
+                error={errorText.toLowerCase().includes("username")}
+                id="login_username_input"
+                size="small"
+                value={loginInput.username}
+                onChange={(e) => {
+                  setLoginInput((prev) => ({
+                    ...prev,
+                    username: e.target.value,
+                  }));
+                }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    fontFamily: "Segoe UI",
+                      fontSize: " 14.5px",
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: errorText.toLowerCase().includes("username")
                         ? "var(--oak-red)"
                         : "#43434239",
-                    borderWidth: errorText.trim().length > 0 ? "2px" : "1.5px",
-                  },
-                  "&.Mui-focused": {
-                    "& .MuiOutlinedInput-notchedOutline": {
-                      borderColor:
-                        errorText.trim().length > 0
+                      borderWidth: errorText.toLowerCase().includes("username")
+                        ? "2px"
+                        : "1.5px",
+                    },
+                    "&.Mui-focused": {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: errorText
+                          .toLowerCase()
+                          .includes("username")
                           ? "var(--oak-red)"
                           : "var(--caramel-brown)",
+                      },
                     },
-                  },
-                  "&:hover:not(.Mui-focused)": {
-                    "& .MuiOutlinedInput-notchedOutline": {
-                      borderColor:
-                        errorText.trim().length > 0
+                    "&:hover:not(.Mui-focused)": {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: errorText
+                          .toLowerCase()
+                          .includes("username")
                           ? "var(--oak-red)"
                           : "#8e4f2d69",
+                      },
                     },
                   },
-                },
-              }}
-              fullWidth
-            />
+                }}
+                fullWidth
+              />
+            </div>
+            {/*Password*/}
+            <div>
+              <p id="login-username-label" className="login-signup-label">
+                Password:{" "}
+              </p>
+              <TextField
+                type={showPasswordVisibility ? "text" : "password"}
+                disabled={isLoading}
+                error={errorText.toLowerCase().includes("password")}
+                id="login_username_input"
+                size="small"
+                value={loginInput.password}
+                onChange={(e) => {
+                  setLoginInput((prev) => ({
+                    ...prev,
+                    password: e.target.value,
+                  }));
+                }}
+                slotProps={{
+                  input: {
+                    //Add search button to the end of the textfield
+                    endAdornment: (
+                      <InputAdornment
+                        position="end"
+                        style={{ marginRight: 10, cursor: "pointer" }}
+                      >
+                        <IconButton
+                          disabled={isLoading}
+                          className="sign-up-login-password-visibility"
+                          onClick={() => {
+                            setShowPasswordVisibility((prev) => !prev);
+                          }}
+                          edge="end"
+                        >
+                          <i
+                            className={
+                              showPasswordVisibility
+                                ? `fa-solid fa-eye`
+                                : `fa-solid fa-eye-slash`
+                            }
+                          ></i>
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    fontFamily: "Segoe UI",
+                      fontSize: " 14.5px",
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: errorText.toLowerCase().includes("password")
+                        ? "var(--oak-red)"
+                        : "#43434239",
+                      borderWidth: errorText.toLowerCase().includes("password")
+                        ? "2px"
+                        : "1.5px",
+                    },
+                    "&.Mui-focused": {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: errorText
+                          .toLowerCase()
+                          .includes("password")
+                          ? "var(--oak-red)"
+                          : "var(--caramel-brown)",
+                      },
+                    },
+                    "&:hover:not(.Mui-focused)": {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: errorText
+                          .toLowerCase()
+                          .includes("password")
+                          ? "var(--oak-red)"
+                          : "#8e4f2d69",
+                      },
+                    },
+                  },
+                }}
+                fullWidth
+              />
+            </div>
             <p id="login-error-text" className="signup-login-error-text">
               {errorText}
             </p>
