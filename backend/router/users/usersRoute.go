5dd51ff07528cff480826e9d2c467ef983be790b
min rui <gohminrui@gmail.com>
2026-01-13 10:14:46 +0800

    Login, Sign Up, Authentication - change user route api with password, add password to login, sign up, upadate user  #9 #6 #3

--- backend/router/users/usersRoute.go
+++ backend/router/users/usersRoute.go
@@ -1,6 +1,7 @@
 package usersRoute
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"errors"
@@ -10,7 +11,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/minrui13/backend/auth"
 	"github.com/minrui13/backend/config"
@@ -39,7 +40,6 @@ func (h *Handler) Router(r *mux.Router) *mux.Router {
 	r.HandleFunc("/updateUser", h.UpdateUser).Methods("PUT")
 	//Get user by user id
 	r.HandleFunc("/{id}", h.GetUserById).Methods("POST")
-
 	return r
 }
 
@@ -51,37 +51,37 @@ func (h *Handler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	//database error 500 status code
 	//same as res.send(500)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		util.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	var users []types.User
+	var usersArr []types.User
 	for rows.Next() {
-		var u types.User
+		var user types.User
 		var created time.Time
 
-		if err := rows.Scan(&u.User_id, &u.Username, &u.DisplayName,
-			&u.Bio, &u.Image_id, &u.Points, &created); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		if err := rows.Scan(&user.User_id, &user.Username, &user.DisplayName,
+			&user.Bio, &user.Image_id, &user.Points, &created); err != nil {
+			util.WriteError(w, http.StatusInternalServerError, err)
 			return
 		}
 
-		u.Created_date = created.Format(time.RFC3339)
-		users = append(users, u)
+		user.Created_date = created.Format(time.RFC3339)
+		usersArr = append(usersArr, user)
 	}
 
 	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		util.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	util.WriteJSON(w, http.StatusOK, users)
+	util.WriteJSON(w, http.StatusOK, usersArr)
 }
 
 // Get user by user_id
 func (h *Handler) GetUserById(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	user := new(types.User)
+	user := new(types.UserIdResult)
 	var created time.Time
 	//get id from params
 	id := mux.Vars(r)["id"]
@@ -103,9 +103,21 @@ func (h *Handler) GetUserById(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//get data from db
-	err = h.db.QueryRow(ctx, `SELECT user_id, username, display_name, image_id, points, created_date FROM users WHERE user_id = $1`, userID).
-		Scan(&user.User_id, &user.Username,
-			&user.DisplayName, &user.Image_id, &user.Points, &created)
+	err = h.db.QueryRow(ctx, `
+		SELECT user_id, username, display_name, bio, image_id, points, created_date
+		FROM users
+		WHERE user_id = $1
+	`, userID).Scan(
+		&user.User_id,
+		&user.Username,
+		&user.DisplayName,
+		&user.Bio,
+		&user.Image_id,
+		&user.Points,
+		&created,
+	)
+
+	println(err)
 	if err != nil {
 		util.WriteError(w, http.StatusNotFound, err)
 		return
@@ -114,10 +126,20 @@ func (h *Handler) GetUserById(w http.ResponseWriter, r *http.Request) {
 	util.WriteJSON(w, http.StatusOK, user)
 }
 
+func (h *Handler) userExists(ctx context.Context, username string) (bool, error) {
+	var exists bool
+	err := h.db.QueryRow(
+		ctx,
+		`SELECT EXISTS (SELECT user_id FROM users WHERE username = $1)`,
+		username,
+	).Scan(&exists)
+
+	return exists, err
+}
+
 // Check if user exists by getting user by username
 func (h *Handler) CheckUserExists(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	var resultType = new(types.CheckUserExists)
 	var payload types.LoginPayload
 	err := json.NewDecoder(r.Body).Decode(&payload)
 
@@ -127,20 +149,21 @@ func (h *Handler) CheckUserExists(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = h.db.QueryRow(ctx, `SELECT EXISTS (SELECT user_id FROM users WHERE username = $1);`, payload.Username).
-		Scan(&resultType.Exists)
+	exists, err := h.userExists(ctx, payload.Username)
 
 	if err != nil {
 		util.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
-	util.WriteJSON(w, http.StatusOK, resultType)
+	util.WriteJSON(w, http.StatusOK, types.CheckUserExists{
+		Exists: exists,
+	})
 }
 
 // Login
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	var user = new(types.User)
+	var result = new(types.LoginResult)
 	var created time.Time
 	var payload types.LoginPayload
 	err := json.NewDecoder(r.Body).Decode(&payload)
@@ -151,20 +174,21 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = h.db.QueryRow(ctx, `SELECT user_id, username, display_name, bio, image_id, points, created_date FROM users WHERE username = $1`, payload.Username).
-		Scan(&user.User_id, &user.Username, &user.DisplayName, &user.Bio, &user.Image_id, &user.Points, &created)
+	err = h.db.QueryRow(ctx, `SELECT user_id, username, display_name, bio, image_id, points, created_date, password FROM users WHERE username = $1`, payload.Username).
+		Scan(&result.User_id, &result.Username, &result.DisplayName, &result.Bio, &result.Image_id, &result.Points, &created, &result.Password)
 
-	if errors.Is(err, sql.ErrNoRows) {
-		util.WriteError(w, http.StatusUnauthorized, errors.New("invalid username"))
+	if errors.Is(err, sql.ErrNoRows) || !auth.ComparePasswords(result.Password, []byte(payload.Password)) {
+		util.WriteError(w, http.StatusBadRequest, fmt.Errorf("Invalid username or password"))
 		return
 	}
+
 	if err != nil {
 		util.WriteError(w, http.StatusNotFound, err)
 		return
 	}
 
 	jwtUser := types.JWTUserInfo{
-		User_id: user.User_id,
+		User_id: result.User_id,
 	}
 	//get JWT secret from .env
 	secret := []byte(config.Envs.JWTSecret)
@@ -177,13 +201,13 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	loginInfo := types.LoginInfo{
-		User_id:      user.User_id,
-		Username:     user.Username,
-		DisplayName:  user.DisplayName,
-		Bio:          user.Bio,
-		Image_id:     user.Image_id,
-		Points:       user.Points,
-		Created_date: user.Created_date,
+		User_id:      result.User_id,
+		Username:     result.Username,
+		DisplayName:  result.DisplayName,
+		Bio:          result.Bio,
+		Image_id:     result.Image_id,
+		Points:       result.Points,
+		Created_date: result.Created_date,
 		Token:        token,
 	}
 
@@ -203,25 +227,24 @@ func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var userID int
-	err = h.db.QueryRow(ctx, `SELECT user_id FROM users WHERE username = $1`, payload.Username).Scan(&userID)
-
-	//check for duplicate
-	if err == nil {
-		util.WriteError(w, http.StatusConflict, errors.New("username already taken"))
-		return
-	}
-
-	//For internal server error, 500
-	if err != pgx.ErrNoRows {
+	//hash password via bcrypt
+	hashedPassword, err := auth.HashPasword(payload.Password)
+	if err != nil {
 		util.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 
+	var userID int
+
 	err = h.db.QueryRow(ctx,
-		`INSERT INTO users (image_id, username, display_name, bio) VALUES ($1, $2, $3, $4) RETURNING  user_id;`,
-		payload.Image_id, payload.Username, payload.DisplayName, payload.Bio,
+		`INSERT INTO users (image_id, username, display_name, bio, password) VALUES ($1, $2, $3, $4, $5) RETURNING  user_id;`,
+		payload.Image_id, payload.Username, payload.DisplayName, payload.Bio, hashedPassword,
 	).Scan(&userID)
+
+	if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
+		util.WriteError(w, http.StatusConflict, errors.New("username already taken"))
+		return
+	}
 
 	//For internal server error, 500
 	if err != nil {
@@ -236,34 +259,46 @@ func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
 // Update user information
 func (h *Handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	user := new(types.UpdateUser)
 	var payload types.UpdateUser
-	err := json.NewDecoder(r.Body).Decode(&payload)
 
-	if err != nil {
-		fmt.Println(err)
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 		util.WriteError(w, http.StatusBadRequest, errors.New("invalid inputs"))
 		return
 	}
 
-	err = h.db.QueryRow(ctx,
-		`UPDATE users SET username = $1, display_name = $2, bio = $3, image_id = $4 
-		WHERE user_id = $5 RETURNING user_id, username, display_name, bio, image_id;`,
-		payload.Username, payload.DisplayName, payload.Bio, payload.Image_id, payload.User_id,
-	).Scan(&user.User_id, &user.Username, &user.DisplayName, &user.Bio, &user.Image_id)
+	var (
+		hashedPassword *string
+	)
 
-	//user_id wrong
-	if err == pgx.ErrNoRows {
-		util.WriteError(w, http.StatusNotFound, errors.New("user not found"))
-		return
+	if payload.Password != "" {
+		//hash password via bcrypt
+		hash, err := auth.HashPasword(payload.Password)
+		if err != nil {
+			util.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+		hashedPassword = &hash
 	}
 
-	//For internal server error, 500
+	var result types.UpdateUser
+	err := h.db.QueryRow(ctx,
+		`UPDATE users SET username = $1, display_name = $2, bio = $3, image_id = $4, password = $5
+		WHERE user_id = $6 RETURNING user_id, username, display_name, bio, image_id;`,
+		payload.Username, payload.DisplayName, payload.Bio, payload.Image_id, hashedPassword, payload.User_id,
+	).Scan(&result.User_id, &result.Username, &result.DisplayName, &result.Bio, &result.Image_id)
+
 	if err != nil {
+		// username duplicate
+		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
+			util.WriteError(w, http.StatusConflict, errors.New("username already taken"))
+			return
+		}
+
+		//server error
 		util.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	util.WriteJSON(w, http.StatusOK, user)
+	util.WriteJSON(w, http.StatusOK, result)
 
 }
--- backend/types/userType.go
+++ backend/types/userType.go
@@ -1,14 +1,18 @@
 package types
 
+import "time"
+
 type SignUpPayload struct {
 	Image_id    int     `json:"image_id"`
 	Username    string  `json:"username"`
 	DisplayName *string `json:"display_name"`
 	Bio         *string `json:"bio"`
+	Password    string  `json:"password"`
 }
 
 type LoginPayload struct {
 	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 type JWTUserInfo struct {
@@ -21,6 +25,7 @@ type UpdateUser struct {
 	DisplayName *string `json:"display_name"`
 	Bio         *string `json:"bio"`
 	Image_id    int     `json:"image_id"`
+	Password    string  `json:"password"`
 }
 
 type User struct {
@@ -31,6 +36,18 @@ type User struct {
 	Image_id     int     `json:"image_id"`
 	Points       int     `json:"points"`
 	Created_date string  `json:"created_date"`
+	Password     string  `json:"password"`
+}
+
+type UserIdResult struct {
+	User_id      int       `json:"user_id"`
+	Username     string    `json:"username"`
+	DisplayName  *string   `json:"display_name"`
+	Bio          *string   `json:"bio"`
+	Image_id     int       `json:"image_id"`
+	Points       int       `json:"points"`
+	Created_date time.Time `json:"created_date"`
+	Password     string    `json:"password"`
 }
 
 type LoginInfo struct {
@@ -42,6 +59,17 @@ type LoginInfo struct {
 	Points       int     `json:"points"`
 	Created_date string  `json:"created_date"`
 	Token        string  `json:"token"`
+}
+
+type LoginResult struct {
+	User_id      int     `json:"user_id"`
+	Username     string  `json:"username"`
+	DisplayName  *string `json:"display_name"`
+	Bio          *string `json:"bio"`
+	Image_id     int     `json:"image_id"`
+	Points       int     `json:"points"`
+	Created_date string  `json:"created_date"`
+	Password     string  `json:"password"`
 }
 
 type CheckUserExists struct {
