536e7987fd04bde6c4f5093ae2262441c3dc527b
min rui <gohminrui@gmail.com>
2026-01-20 19:52:02 +0800

    Posts - fix frontend for posts with cursor #14

--- frontend/src/types/PostType.tsx
+++ frontend/src/types/PostType.tsx
@@ -8,17 +8,17 @@ export type PostDefaultResultType = {
   topic_name: string;
   category_icon: string;
   tag_name: string | null;
-  tag_icon: string |null;
-  tag_description: string |null;
+  tag_icon: string | null;
+  tag_description: string | null;
   title: string;
   content: string;
   created_date: string;
-  vote_id : number;
+  vote_id: number | null;
   upvote_count: number;
   downvote_count: number;
-  vote_status: number|null;
+  vote_status: number | null;
   comment_count: number;
-  bookmark_id: number;
+  bookmark_id: number | null;
   is_bookmarked: boolean;
 };
 
@@ -35,12 +35,43 @@ export type PostPopularityFollowResultType = {
   title: string;
   content: string;
   created_date: string;
-  vote_id: number;
+  vote_id: number | null;
   upvote_count: number;
   downvote_count: number;
-  sum_count: number;
-  vote_status: number|null;
+  sum_count?: number;
+  vote_status: number | null;
   comment_count: number;
-  bookmark_id: number;
+  bookmark_id: number | null;
   is_bookmarked: boolean;
+};
+
+export type DateUpvoteCursor = {
+  created_date: string;
+  upvote_count: number;
+};
+
+export type IDLimitCursorSearch = {
+  user_id: number;
+  limit: number;
+  search: string;
+  cursor: string | null;
+};
+
+export type IDLimitCursorSearchToken = {
+  user_id: number;
+  limit: number;
+  search: string;
+  cursor: string | null;
+  token: string | null;
+};
+
+export type CursorPostDefault = {
+  result: PostDefaultResultType[];
+  cursor: string | null;
+};
+
+
+export type CursorPostDefaultSumVote = {
+  result: PostPopularityFollowResultType[];
+  cursor: string | null;
 };
